2c0945dc40b8259fbda1fe7bb31b9085cf7196a9
hsilan-sui <suihsilan1101@icloud.com>
2025-08-10 18:22:47 +0800

    fix: wrong file path

--- flex/carousel/plansMenuCarousel.js
+++ flex/carousel/plansMenuCarousel.js
@@ -1,6 +1,6 @@
 // 方案列表（基礎方案 與進階方案 總覽 x2）Carousel
-const basicOverviewBubble = require("./basicOverviewBubble");
-const proOverviewBubble   = require("./proOverviewBubble");
+const basicOverviewBubble = require("../basicOverviewBubble");
+const proOverviewBubble   = require("../proOverviewBubble");
 
 module.exports = {
   type: "carousel",
